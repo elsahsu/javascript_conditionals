9ca56eeea95e1a005fbf5fcb897f0d326bc59e64
Elsa Hsu <elsanfhsu@gmail.com>
2018-02-06 20:37:46 +0200

    Quiz 3-1

---
+++ README.md
@@ -0,0 +1,2 @@
+# javascript_conditionals
+Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with rounded corners represent what should be printed to the console.
---
+++ flowcharts.js
@@ -0,0 +1,15 @@
+var balance = 10000000000000.00;
+var checkBalance = true;
+var isActive = true;
+
+if (checkBalance === false) {
+    console.log("Thank you. Have a nice day!");
+} else if (isActive === true && balance > 0) {
+    console.log("Your balance is $" + balance.toFixed(2) + ".");
+} else if (isActive === false) {
+    console.log("Your account is no longer active.");
+} else if (balance === 0) {
+    console.log ("Your account is empty.");
+} else {
+    console.log("Your balance is negative. Please contact bank.");
+}
