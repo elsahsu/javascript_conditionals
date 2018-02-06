b0a8f7b89d815c05fe8f234c9f2e79d93e488005
Elsa Hsu <elsanfhsu@gmail.com>
2018-02-06 20:51:03 +0200

    even or odd

---
+++ even_or_odd.js
@@ -0,0 +1,9 @@
+var number = 88;
+// var remainder = number % 2;
+//console.log(remainder);
+//make number divided by 2 to see if the remainder is zero or not
+if (number % 2 === 1) {
+    console.log ("odd");
+} else {
+    console.log ("even");
+}
